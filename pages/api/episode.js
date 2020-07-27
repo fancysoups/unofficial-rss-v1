645963dac26ea67b52ef8acd7ba8dba419fbc223
shahruz <me@shahruz.com>
2020-07-27 13:34:02 -0700

    episode fix

--- pages/api/episode.js
+++ pages/api/episode.js
@@ -1,6 +1,7 @@
 import connectDB from 'backend/db';
 import User from 'backend/models/User';
 import Feed from 'backend/models/Feed';
+import Episode from 'backend/models/Episode';
 
 export default async (req, res) => {
   await connectDB();
@@ -16,12 +17,7 @@ export default async (req, res) => {
   const active = await user.verifyStitcherPremiumSubscription();
   if (!active) return res.status(403).json({ error: 'Expired subscription.' });
 
-  const feed = await Feed.findOne({ stitcherID: feedID }).lean();
-  if (!feed) return res.status(404).json({ error: 'Feed not found.' });
-
-  const episode = feed.episodes.find(
-    episode => episode.stitcherID == episodeID
-  );
+  const episode = await Episode.findOne({ stitcherID: episodeID }).lean();
   if (!episode) return res.status(404).json({ error: 'Episode not found.' });
   res.status(302).setHeader('Location', episode.url);
   return res.send('');
